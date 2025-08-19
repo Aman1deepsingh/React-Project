e88c70bbdc56510bb60b1febafa5834652da0e11
Aman1deepsingh <Aman1deepsingh>
2025-08-19 11:05:33 +0530

    Added Modules.jsx file

---
+++ my-react-app/src/components/Modules.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Modules() {
+  return (
+    <>
+          <h1>Modules</h1>
+          <p>JavaScript modules allow you to break up your code into separate files.This makes it easier to maintain a code-base.Modules are imported from external files with the import statement.Modules also rely on type="module".</p>
+    </>
+  )
+}
